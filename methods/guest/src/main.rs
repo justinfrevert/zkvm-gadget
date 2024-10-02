a0fcb70d93f0b68d6e7255cdb9b76b72719c1deb
Justin Frevert <justinfrevert@gmail.com>
2024-10-02 01:43:55 -0400

    comment

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -8,11 +8,12 @@ sol! {
     }
 }
 
+// Simply square the input inside of the guest and commit to the abi encoding of its result
 fn main() {
     let input: u32 = env::read();
-
+    // This program simply squares the guest's input
     let num = U256::from(input.pow(2));
-
     let journal = Journal { result: num };
+    // Commit to abi encoding of the result
     env::commit_slice(&journal.abi_encode());
 }
